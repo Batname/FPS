56b013b84e7e463879b7c434f8e7354f2540a272
BatName <dadubinin@gmail.com>
2017-06-16 21:03:17 +0200

    add model

--- Source/FPS/FPSCharacter.h
+++ Source/FPS/FPSCharacter.h
@@ -18,7 +18,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -37,4 +37,5 @@ public:
 
 	UFUNCTION()
 	void StopJump();
+
 };
